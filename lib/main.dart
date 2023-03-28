9cbf4f223c83a850f66a375a38f334e0dbf073fa
hamid313-coder <hamid.hussainy0@gmail.com>
2023-03-28 08:56:10 +0430

    added folders

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("I'm so rich!"),
+              flexibleSpace: Container(
+                  decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Colors.blue,
+                      Colors.cyan,
+                    ]),
+              )),
+            ),
+            body: const Center(
+              child: Image(image: AssetImage("images/rich.jpg")),
+            )));
+  }
+}
